502935f2a9dae9b353de3b3bd791f403dd29aa7e
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2025-10-28 10:07:31 -0400

    config: lint workflow, pre-commit hooks

--- warrior_bot/cli.py
+++ warrior_bot/cli.py
@@ -4,17 +4,16 @@ import click
 
 
 @click.command()
-@click.argument('query', nargs=-1, required=False)
+@click.argument("query", nargs=-1, required=False)
 @click.version_option()
 def main(query):
     """Warrior Bot - Your Wayne State University terminal assistant."""
     if query:
-        question = ' '.join(query)
+        question = " ".join(query)
         click.echo(f"You asked: {question}")
     else:
         click.echo("Warrior Bot ready. Try: warrior-bot <your question>")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
