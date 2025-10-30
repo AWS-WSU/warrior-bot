9a0188bea3d9c3eabd83c06c288134e6c6f1e9f8
Rsb23 <rsbarbu23@gmail.com>
2025-10-29 23:22:33 -0400

    feat: improved about command about.py

--- warrior_bot/core/about.py
+++ warrior_bot/core/about.py
@@ -1,7 +1,35 @@
 import click
 
 
-@click.command()
+@click.command(epilog="See usage section at https://aws-wsu.github.io/warrior-bot/ for more.")
 def about():
     """Official reference for warrior-bot"""
-    click.echo("Official reference for warrior-bot.")
+    click.echo("""
+        Warrior-Bot is a CLI (command line interface) that allows users to find information regarding
+        people, location, academics, and campus life at Wayne State University. Users access these through
+        commands passed to Warrior-Bot. Please see examples below for recommended usage and use help ___
+        for specific commands or topics.
+               
+        warrior-bot where panda-express
+
+               Wayne's very own Panda Express is located on the lowest floor of the Student Center near Starbucks.
+            
+               
+        warrior-bot whois president
+               
+               Wayne State University's current interim president is Richard A. Bierschbach.
+               
+
+        warrior-bot what clubs
+               
+               Wayne State University offers a variety of clubs for all interest levels. Some popular ones are
+                    AWS Club
+                    Formula SAE
+                    Academic Senate
+               
+               
+        warrior-bot go campus-map
+               
+               Opens default browser to https://maps.wayne.edu/
+    """)
+    
