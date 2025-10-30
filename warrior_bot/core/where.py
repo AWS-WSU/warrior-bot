8a5045e8ff67baa4ac7c7be81663a869f5a3e123
Rsb23 <rsbarbu23@gmail.com>
2025-10-30 10:52:23 -0400

    fix: whitespace and line length pre-commit issues

--- warrior_bot/core/where.py
+++ warrior_bot/core/where.py
@@ -1,6 +1,95 @@
 import difflib
+import json
+import os
 
 import click
+
+
+class JSONHandler:
+    """
+    general plan
+    https://www.massinteract.com/wayne-state-university/#24469625p&259.59h&99.76t
+
+    student center
+        food
+            panda-express
+            taco bell
+            ...
+        midtown market
+        game room
+            pool tables
+            ping pong tables
+            gaming center
+    recreation and fitness center
+    UGL - Undergraduate Library
+        Study Spaces
+        Reservable Meeting Rooms
+            go by floor?
+        honors college
+    Purdy Kresge Library
+    STEM Innovation Learning Center
+    Old Main
+        Planetarium
+        Schaver Music Hall
+        Gordon L. Grosscup Anthropolgoy Museum
+    welcome center
+    on-campus living
+        Towers Residential Suites
+        Anthony Wayne Drive Apartments
+            definitely more, might be some popular "unofficial" apartments too
+    Schapp Chemistry Building and Lecture Hall
+    """
+
+    def __init__(self, jsonFileName, jsonFilePath):
+        self.fileName = jsonFileName
+        self.filePath = jsonFilePath
+
+    def fileExists(self, createIfNotExists: bool = True) -> bool:
+        """
+        Check if JSON data file exists already, calls createFile if not
+
+        Parameters:
+            createIfNotExists (bool, default = True):
+                create a JSON file automatically if
+                one is found to not exist
+
+        Returns:
+            bool: True if file exists, False if not
+        """
+
+        if os.path.exists(self.filePath + self.fileName):
+            return True
+        else:
+            if not createIfNotExists:
+                return False
+            else:
+                if self.createFile(self.fileName):
+                    return False
+                else:
+                    raise Exception
+
+    def createFile(self, newFileName: str = "data.json") -> bool:
+        """
+        Creates empty JSON file, will be called from
+        fileExists in most cases
+
+        Parameters:
+            filename (str, default = "data.json"):
+                name of the file to create with file
+                extension
+
+        Returns:
+            bool: True if success, raises Exception if failure
+        """
+
+        try:
+            with open(newFileName, "w") as writeFile:
+                json.dump({}, writeFile, indent=4)
+
+            return True
+        except Exception as e:
+            raise Exception from e
+
 
 # placeholder to test fuzzy matching
 # ideally here we'd actually just pull from a .json file or something similar
